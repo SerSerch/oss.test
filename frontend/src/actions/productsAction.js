6dde91a90b052e0b7fcf165fd400c0766640f28c
SerSerch <serserch@yandex.ru>
2019-02-25 17:17:47 +0200

    Edit getProducts

--- frontend/src/actions/productsAction.js
+++ frontend/src/actions/productsAction.js
@@ -3,10 +3,7 @@ import { createAction } from 'redux-actions';
 export const getAllProductsAction = createAction('[Projects] getAllProductsAction');
 
 export const getAllProducts = (params=[]) => (dispatch) => {
-    Promise.all([
-        fetch('/api/users').then(res => res.json()),
         fetch('/api/products?'+params.join('&')).then(res => res.json())
-    ])
         .then(products => {
             dispatch(getAllProductsAction(products))
         })
